5a8305c21cd23ebcdfb6b5d3941808c543bda2d7
Andrew Jewell Sr. <AutomataControls@gmail.com>
2024-09-23 15:41:04 -0400

    Update install_node_red.sh
    Signed-off-by: Andrew Jewell Sr. <AutomataControls@gmail.com>

--- install_node_red.sh
+++ install_node_red.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Install or update Node.js and Node-RED
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+# Install or update Node.js and Node-RED non-interactively
+echo "Installing or updating Node-RED..."
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi --node20
 
 # Enable Node-RED service to start on boot
+echo "Enabling Node-RED to start on boot..."
 sudo systemctl enable nodered.service
 
 # Start Node-RED service immediately
+echo "Starting Node-RED service..."
 sudo systemctl start nodered.service
 
 echo "Node-RED has been installed or updated, and the service is now enabled to start on boot."
