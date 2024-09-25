77f06440ad5f4399533fb330ec93351815caf6ff
Andrew Jewell Sr. <AutomataControls@gmail.com>
2024-09-25 09:11:12 -0400

    Update set_internet_time_rpi4.sh
    Signed-off-by: Andrew Jewell Sr. <AutomataControls@gmail.com>

--- set_internet_time_rpi4.sh
+++ set_internet_time_rpi4.sh
@@ -13,6 +13,9 @@ sudo systemctl start ntp
 # Force an immediate sync with the internet time server
 sudo ntpd -gq
 
+# Set the system timezone to Eastern Standard Time (EST)
+sudo timedatectl set-timezone America/New_York
+
 # Display the updated date and time
-echo "The system time has been synchronized to the local internet time:"
+echo "The system time has been synchronized to the local internet time (EST):"
 date
