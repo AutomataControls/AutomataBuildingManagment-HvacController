0d39827480d357144ca95f449e42fa59fe8098ed
Andrew Jewell Sr. <AutomataControls@gmail.com>
2024-09-24 23:57:08 -0400

    Update install_node_red.sh
    Signed-off-by: Andrew Jewell Sr. <AutomataControls@gmail.com>

--- install_node_red.sh
+++ install_node_red.sh
@@ -3,18 +3,18 @@
 # Switch to user Automata for the installation
 sudo -u Automata bash << EOF
 
-# Install or update Node.js and Node-RED with interactive prompts
+# Install or update Node.js and Node-RED
 echo "Installing or updating Node-RED..."
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
-# Enable Node-RED service to start on boot
-echo "Enabling Node-RED to start on boot..."
-systemctl --user enable nodered.service
+EOF
 
-# Start Node-RED service immediately
+# Enable Node-RED service to start on boot (system-wide)
+echo "Enabling Node-RED to start on boot..."
+sudo systemctl enable nodered.service
+
+# Start Node-RED service immediately (system-wide)
 echo "Starting Node-RED service..."
-systemctl --user start nodered.service
+sudo systemctl start nodered.service
 
 echo "Node-RED installation completed."
-
-EOF
