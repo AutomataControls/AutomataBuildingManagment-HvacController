ca8d7249c4fb1a3c353a022a3f634c833be24460
Andrew Jewell Sr. <AutomataControls@gmail.com>
2024-09-27 17:23:30 -0400

    Update Open_Node_Red.sh
    Signed-off-by: Andrew Jewell Sr. <AutomataControls@gmail.com>

--- Open_Node_Red.sh
+++ Open_Node_Red.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-xdg-open http://127.0.0.1:1880/
+chromium-browser http://127.0.0.1:1880/ &
+sleep 2  # Optional: wait for a moment to ensure Chromium has launched
 xdg-open http://127.0.0.1:1880/ui
